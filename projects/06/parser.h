e148db4e9f1ec07cdd7121d5ec70731b30054a1f
huttongrabiel <huttonthomas@icloud.com>
2022-01-18 10:56:22 -0800

    removed deleted functions from parser header

--- projects/06/parser.h
+++ projects/06/parser.h
@@ -2,9 +2,7 @@
 #define PARSER 
 
 char *parseAInstruction(char *);
-char *parseCInstruction(char *);
 char *parseLabel(char*);
-char *instructionSelect(char *);
 char *dest(char *);
 char *jump(char *);
 char *comp(char *);
