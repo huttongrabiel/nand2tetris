e2814478941adbb3e8034cf96912072b778df1b9
huttongrabiel <huttonthomas@icloud.com>
2022-01-21 21:18:20 -0800

    layed out work for assembler

--- projects/06/assembler.c
+++ projects/06/assembler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "translator.h"
 
 int main(int argc, char *argv[]) {
   // read the file line by line, skip to next line if we come across "//", 
@@ -8,11 +9,25 @@ int main(int argc, char *argv[]) {
   // to the binary file. Clear instruction string, start over again
 
   FILE *assemblyCode;
-
-  if ((assemblyCode = fopen(argv[1], "r")) == NULL) {
-    fprintf(stderr, "\nERROR: file %s is invalid.\n", argv[1]);
-    return 0;
+  
+  // make sure file is given to assembler
+  if (argc != 2) {
+    fprintf(stderr, "\nERROR: enter command line parameter\n");
+    exit(1);
   }
   
+  // make sure file exists and can be opened
+  if ((assemblyCode = fopen(argv[1], "r")) == NULL) {
+    fprintf(stderr, "\nERROR: file %s is invalid.\n", argv[1]);
+    exit(1);
+  }
+  
+  // read file line by line. clear line after converting to binary
+  // instruction. repeat
 
+
+
+  fclose(assemblyCode);
+
+  return 0;
 }
