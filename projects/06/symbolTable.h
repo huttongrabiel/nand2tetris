9433fdfacc8c849c58c22cf73f916f9640ccf74b
huttongrabiel <huttonthomas@icloud.com>
2022-02-04 19:31:20 -0800

    added function headers for new functions, added extern variables

--- projects/06/symbolTable.h
+++ projects/06/symbolTable.h
@@ -1,9 +1,18 @@
 #ifndef SYMBOL_TABLE
 #define SYMBOL_TABLE
 
+extern char **knownVariables;
+extern int labelCount;
+extern int *labelIndexes;
+extern struct map *programSymbols;
+
 int getProgramSymbolStructSize(FILE *);
 int isLabel(char *);
 int isVariable(char *);
 int isPredefined(char *);
+char *convertIntToChar(int);
+void addLabels(FILE *);
+void addVariables(FILE *);
+int isDefined(char *, char **, int);
 
 #endif
